19afed9d9fd2efd2c070e39c1e47fe810f49f9e6
Kevin Laeremans <tndkevin@hotmail.Com>
2018-05-08 11:28:07 +0200

    workspace

--- www/index.js
+++ www/index.js
@@ -85,7 +85,6 @@ $(document).ready(function () {
                             "</div>" +
                             "</div>";
 
-
                         htmlstring += nextstring;
                         output.innerHTML = htmlstring;
 
