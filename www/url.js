28ff3aba62539098e0a7d9faaa29dc2cb1de29a4
Maarten Van Malcot <maarten.van.malcot@student.ehb.be>
2018-05-18 17:43:42 +0200

    updated url (ready for release 1: 1)

--- www/url.js
+++ www/url.js
@@ -1,4 +1,6 @@
-var url = 'https://excite-us.herokuapp.com/';
+var url3 = 'https://excite-us.herokuapp.com/';
 
 
 var url2 = 'https://on-the-moment-dev.herokuapp.com/'
+
+var url = 'http://exciteus-backend-prod.kcc2fp7rip.eu-west-1.elasticbeanstalk.com/';
